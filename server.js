31ec403cd64d7070cab5ebd068a4f514c924ffd1
Sharanya Garipally <sharanya@Sharanyas-MacBook-Pro.local>
2024-01-31 21:21:13 -0500

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-// Budget API
-
 const express = require('express');
-const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 80;
 
-app.use(cors());
+app.use('/', express.static('public')); 
+
+app.get('/hello', (req, res) => {
+    res.send('Hi World');
+});
 
 const budget = {
     myBudget: [
